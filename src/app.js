30738a551ae2cc208c162da11ce179b52b8f2e96
ankitscode <ankitsinghhp97@gmail.com>
2025-09-15 12:51:28 +0530

    initialed project

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/database');
+const cors = require('cors');
+const route = require('./routes');
+
+
+dotenv.config({ quiet: true }); // load env first
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+// Routes
+app.use('/api', route);
+
+//not found route
+app.use((req, res, next) => {
+    res.status(404).json({ message: 'Route not found' });
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Something went wrong!' });
+});
+
+module.exports = app;
